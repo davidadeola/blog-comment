f7b4c5667a47f458506e32a352314257c385321e
David Adeola <davidolorunsesan15@gmail.com>
2022-12-22 20:19:19 +0200

    create first square component

---
+++ src/components/ticTacToe/card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Square } from "./square";
+
+export const Card = () => {
+  return (
+    <div>
+      <Square />
+    </div>
+  );
+};
---
+++ src/components/ticTacToe/square.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./ticTacToe.css";
+export const Square = () => {
+  return <button className="square">X</button>;
+};
