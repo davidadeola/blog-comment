906a74d2a4c58d14f95dfe104b72ec0f5392dd5e
David Adeola <davidolorunsesan15@gmail.com>
2022-12-29 17:19:19 +0200

    complete tictac toe changes

--- src/components/ticTacToe/game.js
+++ src/components/ticTacToe/game.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Board } from "./board";
 
 export const Game = () => {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history;
   return (
-    <div>
-      <Board />
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol></ol>
+      </div>
     </div>
   );
 };
